a1d983f257898ee9b495a972c67726c922d94c9b
stunxfs <advancestunx@hotmail.com>
2021-01-03 20:37:09 -0400

    all: add 'simple_struct.v'

---
+++ simple_struct.v
@@ -0,0 +1,16 @@
+module main
+
+struct SimpleStruct {
+pub:
+	field1	int
+}
+
+fn (ss SimpleStruct) field1_check() bool {
+	return ss.field1 > 100
+}
+
+fn main() {
+	c := SimpleStruct{150}
+	println("SimpleStruct.field1_check: " + c.field1_check().str())
+}
+
